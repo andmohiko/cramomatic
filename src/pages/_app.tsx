de850d4c8406a9266ac637a880f3b61a83a7cbb3
MorookaKotaro <morookakotaro@team-lab.com>
2022-03-21 15:37:37 +0900

    ログインページ枠実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,40 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Container from "@mui/material/Container";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Noto Sans JP",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="sm">
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;

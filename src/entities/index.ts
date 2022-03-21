cf38333a574cf97328edbad769c3350573b30d6e
MorookaKotaro <morookakotaro@team-lab.com>
2022-03-21 14:17:28 +0900

    tsconfigとeslint設定

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,44 +1,44 @@
-export type Uid = String
+export type Uid = String;
 
-export type UserId = Uid
+export type UserId = Uid;
 
 export interface User {
-  userId: UserId
-  createdAt: Date
-  updatedAt: Date
-  username: string
+  userId: UserId;
+  createdAt: Date;
+  updatedAt: Date;
+  username: string;
 }
 
-export type CardId = Uid
+export type CardId = Uid;
 
 export type BuyingPrice = {
-  highest: number
-  lowest: number
-}
+  highest: number;
+  lowest: number;
+};
 
 export enum Rarity {
   Common = 'common',
-  Rare = 'rare'
+  Rare = 'rare',
 }
 
 export interface Card {
-  cardId: CardId
-  buyingPrice: BuyingPrice
-  cardImageUrl: URL
-  createdAt: Date
-  name: string
-  rarity: Rarity
-  regulation: string
-  version: string
-  updatedAt: Date
+  cardId: CardId;
+  buyingPrice: BuyingPrice;
+  cardImageUrl: URL;
+  createdAt: Date;
+  name: string;
+  rarity: Rarity;
+  regulation: string;
+  version: string;
+  updatedAt: Date;
 }
 
-export type ShopId = Uid
+export type ShopId = Uid;
 
 export interface Shop {
-  shopId: ShopId
-  address: string
-  createdAt: Date
-  shopname: string
-  updatedAt: Date
+  shopId: ShopId;
+  address: string;
+  createdAt: Date;
+  shopname: string;
+  updatedAt: Date;
 }
--- src/hooks/useCards.ts
+++ src/hooks/useCards.ts
@@ -1,33 +1,33 @@
-import { useState, useEffect } from 'react'
-import { collection, query, orderBy, Firestore, DocumentData, Timestamp } from 'firebase/firestore'
-import { useCollection } from 'react-firebase-hooks/firestore'
-import { Card } from '../entities'
-import { isDefined } from '../utils/type'
-import { convertDate } from '../utils/date'
+import { useState, useEffect } from 'react';
+import { collection, query, orderBy, Firestore, DocumentData, Timestamp } from 'firebase/firestore';
+import { useCollection } from 'react-firebase-hooks/firestore';
+import { Card } from '../entities';
+import { isDefined } from '../utils/type';
+import { convertDate } from '../utils/date';
 
-const CardsCollection = 'cards'
+const CardsCollection = 'cards';
 
 export const useCards = (db: Firestore) => {
-  const [cards, setCards] = useState<Card[]>([])
-  const [value, loading, error] = useCollection(query(collection(db, CardsCollection), orderBy('name')))
+  const [cards, setCards] = useState<Card[]>([]);
+  const [value, loading, error] = useCollection(query(collection(db, CardsCollection), orderBy('name')));
 
   useEffect(() => {
-    if (!value) return
+    if (!value) return;
     const cards = value.docs
       .map((doc) => {
-        return cardFactory(doc)
+        return cardFactory(doc);
       })
-      .filter(isDefined)
-    setCards(cards)
-  }, [value])
+      .filter(isDefined);
+    setCards(cards);
+  }, [value]);
 
-  return cards
-}
+  return cards;
+};
 
 const cardFactory = (doc: DocumentData): Card | undefined => {
-  const card = doc.data()
+  const card = doc.data();
   if (!card) {
-    return
+    return;
   }
 
   return {
@@ -39,6 +39,6 @@ const cardFactory = (doc: DocumentData): Card | undefined => {
     rarity: card.rarity,
     regulation: card.regulation,
     updatedAt: convertDate(card.updatedAt),
-    version: card.version
-  }
-}
+    version: card.version,
+  };
+};
